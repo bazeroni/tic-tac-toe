63a2eb16c2bec1f5f1fc08b50f2126c5deb13917
Sebastian <44552816+bazeroni@users.noreply.github.com>
2021-04-27 22:30:32 -0400

    Create ttt_functions.hpp

---
+++ ttt_functions.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+bool is_winner(std::vector<std::string> board, std::string player);
+bool filled_up(std::vector<std::string> board);
+void introduction();
+//void take_turn(std::string player, int turn);
+void update_board(std::vector<std::string> board);
+//void change_player(std::string player);
+void end_game();
+//void set_position(std::vector<std::string> board, int turn, std::string player);
